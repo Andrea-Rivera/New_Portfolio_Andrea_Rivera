bcb055c470e31ad69c37327f98e9868986cc7af2
Andrea <andita54@hotmail.com>
2019-10-20 06:10:15 +1000

    New Andrea Rivera's Portfolio

---
+++ scripts/myScripts.js
@@ -0,0 +1,7 @@
+//JS FILE TO PRODUCE AN ANIMATION ON SCROLL
+$(document).ready(function() {
+    "use strict";
+//When the document(Webpage is ready) execute the code.
+AOS.init();
+}); //end document ready
+
